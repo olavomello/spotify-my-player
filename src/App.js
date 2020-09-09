311b6b591784426ee7afec80bbae2f5459dd266c
Olavo Mello <olavo@agenciadix.com.br>
2020-09-09 01:14:18 -0300

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faPlay,
+  faStop,
+  faForward,
+  faBackward,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+
+library.add(faPlay, faStop, faForward, faBackward);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Player">
+        <header>
+          <h4>My Spotify Player</h4>
+        </header>
+        <aside>
+          <div className="Playing">
+            <h1>Música A</h1>
+          </div>
+          <div className="Nav">
+            <button className="prev">
+              <FontAwesomeIcon icon="backward" />
+            </button>
+            <button className="play">
+              <FontAwesomeIcon icon="play" />
+            </button>
+            <button className="stop">
+              <FontAwesomeIcon icon="stop" />
+            </button>
+            <button className="next">
+              <FontAwesomeIcon icon="forward" />
+            </button>
+          </div>
+          <div className="Playlist">
+            <ol>
+              <li>Música A</li>
+              <li>Música B</li>
+              <li>Música C</li>
+            </ol>
+          </div>
+        </aside>
+      </div>
     </div>
   );
 }
