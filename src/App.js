b474d20a60a1d8c2475114c85d84d2999f894f00
Olavo Mello <olavo@agenciadix.com.br>
2020-09-09 19:44:23 -0300

    Saving last changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,55 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./App.css";
 
+// Fontaweson
 library.add(faPlay, faStop, faForward, faBackward, faMusic);
+
+// Funcs
+function PlayerPrev() {
+  console.log("Player : Prev");
+}
+function PlayerPlay() {
+  console.log("Player : Play");
+}
+function PlayerStop() {
+  console.log("Player : Stop");
+}
+function PlayerNext() {
+  console.log("Player : Next");
+}
+/*
+function PlayerPause(){
+  console.log("Player : Pause");
+}
+*/
+function PlayerInit() {
+  let list = "";
+
+  // Fakeplaylist **
+  let playlist = [
+    {
+      name: "Music 1",
+      url: "https://music1.com",
+      cover: "https://music1.com/cover.jpg",
+    },
+    {
+      name: "Music 2",
+      url: "https://music2.com",
+      cover: "https://music2.com/cover.jpg",
+    },
+    {
+      name: "Music 3",
+      url: "https://music3.com",
+      cover: "https://music3.com/cover.jpg",
+    },
+  ];
+
+  // Read and apply playlist
+  playlist.map((row) => {
+    console.log(row);
+    list += "<li>" + row.name + "</li>";
+  });
+}
 
 function App() {
   return (
@@ -25,30 +73,26 @@ function App() {
             <h2>
               <FontAwesomeIcon icon="music" />
             </h2>
-            <h1>Música A</h1>
+            <h1>Loading...</h1>
             <h6>00:00:00</h6>
             <div className="Reflex"></div>
           </div>
           <div className="Nav">
-            <button className="prev">
+            <button className="prev" title="Anterior" onClick={PlayerPrev}>
               <FontAwesomeIcon icon="backward" />
             </button>
-            <button className="play">
+            <button className="play" title="Play" onClick={PlayerPlay}>
               <FontAwesomeIcon icon="play" />
             </button>
-            <button className="stop">
+            <button className="stop" title="Stop" onClick={PlayerStop}>
               <FontAwesomeIcon icon="stop" />
             </button>
-            <button className="next">
+            <button className="next" title="Próxima" onClick={PlayerNext}>
               <FontAwesomeIcon icon="forward" />
             </button>
           </div>
           <div className="Playlist">
-            <ol>
-              <li>Música A</li>
-              <li>Música B</li>
-              <li>Música C</li>
-            </ol>
+            <ol></ol>
           </div>
         </aside>
       </div>
