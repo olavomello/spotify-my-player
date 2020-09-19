15248e131f94f943ab3b10671b02320800502e56
Olavo Mello <olavo@agenciadix.com.br>
2020-09-19 17:14:25 -0300

    Update last changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Media, Player, controls } from "react-media-player";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faPlay,
@@ -13,54 +14,58 @@ import "./App.css";
 // Fontaweson
 library.add(faPlay, faStop, faForward, faBackward, faMusic);
 
-// Funcs
-function PlayerPrev() {
-  console.log("Player : Prev");
-}
-function PlayerPlay() {
-  console.log("Player : Play");
-}
-function PlayerStop() {
-  console.log("Player : Stop");
-}
-function PlayerNext() {
-  console.log("Player : Next");
-}
-/*
-function PlayerPause(){
-  console.log("Player : Pause");
-}
-*/
-function PlayerInit() {
-  let list = "";
+function App() {
+  const [isPlaying, setPlaying] = useState(false);
+  const [playingName, setPlayingName] = useState(0);
 
   // Fakeplaylist **
   let playlist = [
     {
-      name: "Music 1",
-      url: "https://music1.com",
-      cover: "https://music1.com/cover.jpg",
+      id: 1,
+      name: "In the Moment of Inspiration",
+      url:
+        "https://www.wonderplugin.com/wp-content/uploads/2014/03/In-the-Moment-of-Inspiration.mp3",
+      cover:
+        "https://www.wonderplugin.com/wp-content/uploads/2014/03/Evening100.jpg",
     },
     {
-      name: "Music 2",
-      url: "https://music2.com",
-      cover: "https://music2.com/cover.jpg",
+      id: 2,
+      name: "Peaceful Dawn",
+      url:
+        "https://www.wonderplugin.com/wp-content/uploads/2014/03/Peaceful-Dawn.mp3",
+      cover:
+        "https://www.wonderplugin.com/wp-content/uploads/2014/03/Island100.jpg",
     },
     {
-      name: "Music 3",
-      url: "https://music3.com",
-      cover: "https://music3.com/cover.jpg",
+      id: 3,
+      name: "Photos and Memories",
+      url:
+        "https://www.wonderplugin.com/wp-content/uploads/2014/03/Photos-and-Memories.mp3",
+      cover:
+        "https://www.wonderplugin.com/wp-content/uploads/2014/03/Sunrise100.jpg",
     },
   ];
 
-  // Read and apply playlist
-  playlist.map((row) => {
-    console.log(row);
-    list += "<li>" + row.name + "</li>";
-  });
-}
+  // Funcs
+  function PlayerSet(id) {
+    console.log("Musica : " + id);
+    setPlayingName("Musica : " + id);
+  }
+  function PlayerPrev() {
+    console.log("Player : Prev");
+  }
+  function PlayerPlay() {
+    console.log("Player : Play");
+    setPlaying(true);
+  }
+  function PlayerStop() {
+    console.log("Player : Stop");
+    setPlaying(false);
+  }
+  function PlayerNext() {
+    console.log("Player : Next");
+  }
 
-function App() {
   return (
     <div className="App">
       <div className="Player">
@@ -69,11 +74,11 @@ function App() {
         </header>
         <aside>
           <div className="Playing">
-            <h5>Stopped</h5>
+            <h5>{isPlaying ? "Playing" : "Stopped"}</h5>
             <h2>
               <FontAwesomeIcon icon="music" />
             </h2>
-            <h1>Loading...</h1>
+            <h1>{isPlaying ? playingName : "Loading..."}</h1>
             <h6>00:00:00</h6>
             <div className="Reflex"></div>
           </div>
@@ -81,18 +86,27 @@ function App() {
             <button className="prev" title="Anterior" onClick={PlayerPrev}>
               <FontAwesomeIcon icon="backward" />
             </button>
-            <button className="play" title="Play" onClick={PlayerPlay}>
-              <FontAwesomeIcon icon="play" />
-            </button>
-            <button className="stop" title="Stop" onClick={PlayerStop}>
-              <FontAwesomeIcon icon="stop" />
-            </button>
+            {!isPlaying ? (
+              <button className="play" title="Play" onClick={PlayerPlay}>
+                <FontAwesomeIcon icon="play" />
+              </button>
+            ) : (
+              <button className="stop" title="Stop" onClick={PlayerStop}>
+                <FontAwesomeIcon icon="stop" />
+              </button>
+            )}
             <button className="next" title="Próxima" onClick={PlayerNext}>
               <FontAwesomeIcon icon="forward" />
             </button>
           </div>
           <div className="Playlist">
-            <ol></ol>
+            <ol>
+              {playlist.map((row) => (
+                <li>
+                  <button>{row.name}</button>
+                </li>
+              ))}
+            </ol>
           </div>
         </aside>
       </div>
